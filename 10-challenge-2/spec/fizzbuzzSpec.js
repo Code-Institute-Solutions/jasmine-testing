e25d74ba8263243a280a1fe3de10903cfa79aa8d
NielMc <nielmce@gmail.com>
2018-02-14 11:36:23 +0000

    added comments to spec

--- 10-challenge-2/spec/fizzbuzzSpec.js
+++ 10-challenge-2/spec/fizzbuzzSpec.js
@@ -1,12 +1,16 @@
 describe("My fizzBuzz function", function() {
+    // A test suite begins with a call to the global Jasmine function describe with two parameters: a string and a function.
     
     beforeEach(function() {
         fizzbuzz = new fizzBuzz();
     });
     
     describe("Returns number, fizz, buzz or fizzbuzz", function() {
-        
+        // Specs are defined by calling the global Jasmine function it
         it("should exist", function() {
+            // Expectations are built with the function expect which takes a value, called the actual. 
+            // Each matcher implements a boolean comparison between the actual value and the expected value.
+            // Any matcher can evaluate to a negative assertion by chaining the call to expect with a not before calling the matcher.
             expect(fizzBuzz).toBeDefined();
         });
         
